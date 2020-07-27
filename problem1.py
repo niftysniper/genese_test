80bed4a0bd65c8b124be9df0f6efebe0734a17f3
niftysniper <niftynabin@gmail.com>
2020-07-27 20:29:43 +0545

    Add files via upload
    took help from internet.

---
+++ problem1.py
@@ -0,0 +1,12 @@
+L, t_max, p_max = 1000, 0, 0  # L must be an even integer
+
+for p in range(L//4*2, L+1, 2):
+    t = 0
+    for a in range(2, int(p/3.4142) + 1):
+        if p*(p - 2*a) % (2*(p - a)) == 0:
+            t += 1
+            if t >= t_max:
+                t_max, p_max = t, p
+
+print("Maximum perimeter, p <=", L, "is", p_max)
+
